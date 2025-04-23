119a8a6936dda75dc936523311615e314df02e83
Rodolfo Cunhasque Faedo <faedorodolfo.programming@gmail.com>
2025-04-23 01:24:02 -0300

    Some modifications

--- src/main/java/com/rodolfocf/client/ClientApplication.java
+++ src/main/java/com/rodolfocf/client/ClientApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Fe
 public class ClientApplication {
 
 	public static void main(String[] args) {
